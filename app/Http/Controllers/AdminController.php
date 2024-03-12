d43f13441713c3f412b30c01e48398dba3a64b7f
devFarjanur <farjanur15-2956@diu.edu.bd>
2024-03-12 17:19:51 +0600

    create admin login

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
 
 class AdminController extends Controller
 {
     public function AdminDashboard(){
         return view('admin.index');
     }  // end method
+
+    public function AdminLogout(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }  // end method
+
+    public function AdminLogin(){
+        return view('admin.admin_login');
+    }  // end method
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,8 @@ require __DIR__.'/auth.php';
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 });
 
 
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
