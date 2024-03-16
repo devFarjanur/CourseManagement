f7fe8b860ee6bc81e71163d6149b07cfc4dd7fd4
devFarjanur <farjanur15-2956@diu.edu.bd>
2024-03-16 16:56:12 +0600

    admin profile create

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
+use App\Models\User;
 
 class AdminController extends Controller
 {
@@ -29,7 +30,9 @@ class AdminController extends Controller
 
 
     public function AdminProfile(){
-        return view('admin.admin_profile_view');
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_profile_view',compact('profileData'));
     }  // end method
 
 
